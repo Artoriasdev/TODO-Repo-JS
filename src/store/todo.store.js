9d8167eb694b1ae025145f28722080daf11620b8
Artoriasdev <fmontenegro2207@gmail.com>
2025-11-27 00:45:56 -0500

    first commit

---
+++ src/store/todo.store.js
@@ -0,0 +1,104 @@
+import { Todo } from "../todos/models/todo.model";
+
+//simula que sea una enumeracion
+const Filters = {
+  All: "all",
+  Completed: "Completed",
+  Pending: "Pending",
+};
+
+const state = {
+  todos: [
+    new Todo("Piedra del alma"),
+    new Todo("Piedra del infinito"),
+    new Todo("Piedra del tiempo"),
+    new Todo("Piedra del poder"),
+    new Todo("Piedra del realidad"),
+  ],
+  filter: Filters.All,
+};
+
+const initStore = () => {
+  console.log(state);
+  console.log("initStore");
+};
+
+const loadStore = () => {
+  throw new Error("Not implemented");
+};
+
+const getTodos = (filter = Filters.All) => {
+  switch (filter) {
+    case Filters.All:
+      return [...state.todos];
+
+    case Filters.Completed:
+      return state.todos.filter((todo) => todo.done);
+
+    case Filters.Pending:
+      return state.todos.filter((todo) => !todo.done);
+
+    default:
+      throw new Error(`Option ${filter} is not valid.`);
+  }
+};
+
+/**
+ *
+ * @param {String} descripcion
+ */
+const addTodo = (description) => {
+  if (!description) throw new Error("Description is required");
+  state.todos.push(new Todo(description));
+};
+
+/**
+ *
+ * @param {String} todoId
+ */
+
+const toggleTodo = (todoId) => {
+  state.todos = state.todos.map((todo) => {
+    if (todo.id === todoId) {
+      todo.done = !todo.done;
+    }
+    return todo;
+  });
+};
+
+/**
+ *
+ * @param {String} todoId
+ */
+
+const deleteTodo = (todoId) => {
+  state.todos = state.todos.filter((todo) => todo.id !== todoId);
+};
+
+const deleteCompleted = () => {
+  state.todos = state.todos.filter((todo) => todo.done);
+};
+
+/**
+ *
+ * @param {Filters} newFilter
+ */
+const setFilter = (newFilter = Filters.All) => {
+  state.filter = newFilter;
+};
+
+const getCurrentFilter = () => {
+  return state.filter;
+};
+
+export default {
+  initStore,
+  loadStore,
+  addTodo,
+  toggleTodo,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter,
+  getTodos,
+};
